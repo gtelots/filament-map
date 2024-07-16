f5953f41f510b7d9f0d984741145300f791ec78b
tung.tt <tung.tt@ots.vn>
2024-07-16 17:15:32 +0700

    Update geojson

--- resources/monorepo/packages/react-map/src/components/DynamicLayer.tsx
+++ resources/monorepo/packages/react-map/src/components/DynamicLayer.tsx
@@ -1,23 +1,32 @@
-import { WMSTileLayer } from "react-leaflet"
+import { WMSTileLayer } from 'react-leaflet'
+import { GeoJSON } from '../components/GeoJSON'
 
 type TDynamicLayerProps = {
   type: 'wms' | 'geojson'
 }
 
-function DynamicLayer(props: TDynamicLayerProps){
+function DynamicLayer(props: TDynamicLayerProps) {
   const { type, ...other } = props
 
-  if(type === 'wms'){
+  if (type === 'wms') {
     const opts = {
       url: '',
       format: 'image/png',
       transparent: true,
-      ...other
+      ...other,
     }
 
     return <WMSTileLayer {...opts} />
   }
 
+  if (type === 'geojson') {
+    const opts =  {
+      // @ts-ignore
+      data: JSON.parse(other.data),
+      pmIgnore: true
+    }
+    return <GeoJSON {...opts}/>
+  }
 
   return null
 }
